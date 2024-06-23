1fc25a04a9f2e2745efe2c36da68fd6e37e71b5e
mapersico <marco.persico.94@gmail.com>
2024-06-23 02:08:44 -0300

    Fix port issues

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,10 @@ import { AppModule } from './core/app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors({ origin: process.env['ORIGIN'], credentials: true });
   app.setGlobalPrefix('api/v1');
 
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
 
 bootstrap();
